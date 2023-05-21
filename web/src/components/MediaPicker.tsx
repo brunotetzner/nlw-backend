5571ead06c5e6a908c5d744ffe81bb1c5fa467d1
Bruno Tetzner <brunoltetzner@gmail.com>
2023-05-21 20:57:29 -0300

    finished web

---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,38 @@
+'use client' // permite usar javascript em telas com algum tipo de interatividade
+import { ChangeEvent, useState } from 'react'
+
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    const previewURL = URL.createObjectURL(files[0])
+    setPreview(previewURL)
+  }
+  return (
+    <>
+      <input
+        type="file"
+        id="media"
+        className="invisible"
+        name="coverUrl"
+        onChange={onFileSelected}
+        accept="image/*"
+      />
+
+      {preview && (
+        // eslint-disable-next-line
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+    </>
+  )
+}
